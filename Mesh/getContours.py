e74275782059a241a0e1f6ff54f345ed3fff3dc2
Mariana Mtz A <49769695+mtzmarianaa@users.noreply.github.com>
2022-05-26 10:26:45 -0400

    Playing with CGAL + OpenCV

--- Mesh/getContours.py
+++ Mesh/getContours.py
@@ -4,16 +4,24 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__))) 
 
-im = cv.imread('ommatidium_diagram.jpg')
+im = cv.imread('ommatidium_diagram2.jpg')
 cv.imshow('image',im)
 imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
 ret, thresh = cv.threshold(imgray, 127, 255, 0)
 contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-# print(len(contours))
-cnt_vec = np.arange(-8,0)
+
+list_points = np.array( contours[1] )
+for k in range(2, 10):
+    np.append(list_points, contours[k] )
+    
+print( list_points.shape  )
+#print(list_points)
+
+# Drawing the contours on top of the original image
+cnt_vec = np.arange(-9 ,0)
 for cnt_i in cnt_vec: 
     cnt = contours[cnt_i]
-    cv.drawContours(im, [cnt], 0, (255, 255, 0), 2 )
+    cv.drawContours(im, [cnt], 0, (255, 255, 0), 3 )
 cv.imshow('img', im)
 cv.imwrite('contours.jpg', im)  
 cv.waitKey()
