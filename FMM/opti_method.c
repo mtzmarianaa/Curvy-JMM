294f8336ea7649198e1557e06bf1604f0826484a
Mariana Mtz A <49769695+mtzmarianaa@users.noreply.github.com>
2022-09-02 18:53:50 -0400

    Update opti_method.c

--- FMM/opti_method.c
+++ FMM/opti_method.c
@@ -24,18 +24,18 @@ double eikApproxLin(double T0, double T1, double lambda, double x0[2], double x1
     xLamMinxHat[0] = 0;
     xLamMinxHat[1] = 0;
     min1Lambda = 1-lambda;
-    printf("   aux printing eikApproxLin\n");
-    printf("            1-lambda = %fl\n", min1Lambda);
+    // printf("   aux printing eikApproxLin\n");
+    // printf("            1-lambda = %fl\n", min1Lambda);
     scalar_times_2vec( min1Lambda, x0, firstPartxLambda ); // (1-lambda)*x0
-    printf("            (1-lambda)*x0 = (%fl,%fl)\n", firstPartxLambda[0], firstPartxLambda[1]);
+    // printf("            (1-lambda)*x0 = (%fl,%fl)\n", firstPartxLambda[0], firstPartxLambda[1]);
     scalar_times_2vec(lambda, x1, secondPartxLambda); // lambda*x1
-    printf("            lambda*x1 = (%fl,%fl)\n", secondPartxLambda[0], secondPartxLambda[1]);
+    // printf("            lambda*x1 = (%fl,%fl)\n", secondPartxLambda[0], secondPartxLambda[1]);
     vec2_addition( firstPartxLambda, secondPartxLambda, xlambda ); // (1-lambda)*x0 + lambda*x1
-    printf("            xlambda = (%fl,%fl)\n", xlambda[0], xlambda[1]);
+    // printf("            xlambda = (%fl,%fl)\n", xlambda[0], xlambda[1]);
     vec2_substraction( xlambda, xHat, xLamMinxHat ); // xlambda - xHat
-    printf("            xlambda-xHat = (%fl, %fl)\n", xLamMinxHat[0], xLamMinxHat[1]);
+    // printf("            xlambda-xHat = (%fl, %fl)\n", xLamMinxHat[0], xLamMinxHat[1]);
     normAux = l2norm( xLamMinxHat ); //   || xlambda - xHat ||
-    printf("            norm(xlambda-xHat) = %fl\n", normAux);
+    // printf("            norm(xlambda-xHat) = %fl\n", normAux);
 
     gApprox = min1Lambda*T0 + lambda*T1 + indexRef*normAux;
 
@@ -87,17 +87,17 @@ double secant_2D(double lambda0, double lambda1, double T0, double T1, double x0
     double lam, gPrime0, gPrime1;
 
     gPrime1 = gPrime(T0, T1, lambda1, x0, x1, xHat, indexRef);
-    printf("Initial, with lambda = %fl   the value of gPrime is: %fl\n", lambda0, gPrime(T0, T1, lambda0, x0, x1, xHat, indexRef));
-    printf("Initial, with lambda = %fl   the value of gPrime is: %fl\n", lambda1, gPrime1);
+    // printf("Initial, with lambda = %fl   the value of gPrime is: %fl\n", lambda0, gPrime(T0, T1, lambda0, x0, x1, xHat, indexRef));
+    // printf("Initial, with lambda = %fl   the value of gPrime is: %fl\n", lambda1, gPrime1);
     
     while(k < maxIter & fabs(gPrime1)>tol){
         // u
         gPrime0 = gPrime(T0, T1, lambda0, x0, x1, xHat, indexRef);
         gPrime1 = gPrime(T0, T1, lambda1, x0, x1, xHat, indexRef);
         lam = lambda1 - gPrime1*(lambda1 - lambda0)/( gPrime1 - gPrime0 );
-        printf("In this iteration lambda_i has value of %fl", lam);
-        printf("    |  value of T(xhat): %fl   with %fl as index of regraction\n", eikApproxLin(T0, T1, lam, x0, x1, xHat, indexRef), indexRef );
-        printf("    |   In this iteration the value of gPrime is: %fl \n", gPrime(T0, T1, lam, x0, x1, xHat, indexRef));
+        // printf("In this iteration lambda_i has value of %fl", lam);
+        // printf("    |  value of T(xhat): %fl   with %fl as index of regraction\n", eikApproxLin(T0, T1, lam, x0, x1, xHat, indexRef), indexRef );
+        // printf("    |   In this iteration the value of gPrime is: %fl \n", gPrime(T0, T1, lam, x0, x1, xHat, indexRef));
         lambda0 = lambda1;
         lambda1 = lam;
         k ++;
@@ -109,8 +109,8 @@ double secant_2D(double lambda0, double lambda1, double T0, double T1, double x0
     if(lambda1 > 1){
         lambda1 = 1;
     }
-    printf("Optimum lambda found %fl\n", lambda1);
-    printf("With gPrime value of: %fl\n", gPrime(T0, T1, lambda1, x0, x1, xHat, indexRef));
+    // printf("Optimum lambda found %fl\n", lambda1);
+    // printf("With gPrime value of: %fl\n", gPrime(T0, T1, lambda1, x0, x1, xHat, indexRef));
     return lambda1;
 }
 
@@ -199,33 +199,33 @@ void projectedGradientDescent(double optimizers[2], double T0, double T1, double
     iter = 0;
     optimizers[0] = 1.0;
     optimizers[1] = 1.0; // initial value of the optimizers
-    printf("\n\nIteration %d\n", iter);
-    printf("Optimizers at this step: %fl    , %fl\n", optimizers[0], optimizers[1]);
+    // printf("\n\nIteration %d\n", iter);
+    // printf("Optimizers at this step: %fl    , %fl\n", optimizers[0], optimizers[1]);
     // we have to calculate the current gradient
     gradient_2Regions(grad, T0, T1, optimizers[0], optimizers[1], x0, x1, x2, xHat, indexRef_01, indexRef_02);
-    printf("Gradient: (%fl, %fl)\n", grad[0], grad[1]);
+    // printf("Gradient: (%fl, %fl)\n", grad[0], grad[1]);
     // steepest descent and calculation of the projected gradient
     projection01Cube(grad, projectedGradient);
-    printf("Projected gradient: (%fl, %fl)\n", projectedGradient[0], projectedGradient[1]);
+    // printf("Projected gradient: (%fl, %fl)\n", projectedGradient[0], projectedGradient[1]);
 
     // iteration part
     while( l2norm(projectedGradient) > tol & iter < maxIter  ){
         iter ++;
-        printf("\n\nIteration %d\n", iter);
+        // printf("\n\nIteration %d\n", iter);
         // we have to calculate the current gradient
-        printf("Projected gradient: (%fl, %fl)  with norm %fl\n", projectedGradient[0], projectedGradient[1], l2norm(projectedGradient));
+        // printf("Projected gradient: (%fl, %fl)  with norm %fl\n", projectedGradient[0], projectedGradient[1], l2norm(projectedGradient));
         t = backtracking(optimizers, grad, T0, T1, x0, x1, x2, xHat, indexRef_01, indexRef_02);
         scalar_times_2vec(t, grad, step);
-        printf("This steps %fl with direction: (%fl, %fl)\n", t, step[0], step[1]);
+        // printf("This steps %fl with direction: (%fl, %fl)\n", t, step[0], step[1]);
         vec2_substraction( optimizers, step, yk );
-        printf("Without projecting back to the domain, the next optimizers would be   %fl   %fl\n", yk[0], yk[1]);
+        // printf("Without projecting back to the domain, the next optimizers would be   %fl   %fl\n", yk[0], yk[1]);
         // then we need to project it back to the feasible set
         projection01Cube(yk, optimizers);
-        printf("Optimizers at this step: %fl    , %fl\n", optimizers[0], optimizers[1]);
+        // printf("Optimizers at this step: %fl    , %fl\n", optimizers[0], optimizers[1]);
         Tcurrent = eikApproxLin_2Regions(T0, T1, optimizers[0], optimizers[1], x0, x1, x2, xHat, indexRef_01, indexRef_02);
-        printf("Current value of T: %fl\n", Tcurrent);
+        // printf("Current value of T: %fl\n", Tcurrent);
         gradient_2Regions(grad, T0, T1, optimizers[0], optimizers[1], x0, x1, x2, xHat, indexRef_01, indexRef_02);
-        printf("Gradient: (%fl, %fl)  with norm %fl\n", grad[0], grad[1], l2norm(grad));
+        // printf("Gradient: (%fl, %fl)  with norm %fl\n", grad[0], grad[1], l2norm(grad));
         // steepest descent and calculation of the projected gradient
         projection01Cube(grad, projectedGradient);
         if(optimizers[0] == 0 & optimizers[1] == 0){
@@ -233,7 +233,7 @@ void projectedGradientDescent(double optimizers[2], double T0, double T1, double
         }
     }
 
-    printf("Optimizers at end of iteration: %fl    , %fl\n", optimizers[0], optimizers[1]);
+    // printf("Optimizers at end of iteration: %fl    , %fl\n", optimizers[0], optimizers[1]);
 
 }
 
